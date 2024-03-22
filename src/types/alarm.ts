86fa5a7df47401888cd9443b32c64b54e95545c5
jeong jun <jungjun3047@naver.com>
2024-03-22 23:18:31 +0900

    fix: alarm type

--- src/types/alarm.ts
+++ src/types/alarm.ts
@@ -1,7 +1,7 @@
-import { UserType } from "./user";
+import { SimpleUserType } from "./user";
 
 export interface AlarmType {
     id: number;
     question: string;
-    asker: UserType;
+    asker: SimpleUserType;
 }
