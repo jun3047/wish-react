d4bdc5b6e2542024e5b2311b2709122e4675e885
jeong jun <jungjun3047@naver.com>
2024-03-26 18:31:27 +0900

    feat(friend): friend page 퍼블리싱

---
+++ src/components/FriendSection.tsx
@@ -0,0 +1,164 @@
+/** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import handleNative from '../native';
+import useUser from '../hooks/useUser';
+import { SimpleUserType, UserType } from '../types/user';
+import makeUserSimple from '../utils/MakeUserSimple';
+import { Logo } from '../pages/HomePage';
+import { friendApi } from '../apis';
+
+
+interface FriendSectionProps {
+    title: string;
+    data: SimpleUserType[];
+}
+
+const FriendSection: React.FC<FriendSectionProps> = ({ title, data }) => {
+  const [maxDisplayCount, setMaxDisplayCount] = useState(2);
+
+  const [user, setUser] = useUser()
+
+  if(!user) return <Logo>대기중</Logo>
+
+  const handleShowMore = () => {
+    setMaxDisplayCount((prevCount) => Math.min(prevCount + 10, data.length));
+  };
+
+  const beFriend = async (targetUser: SimpleUserType) => {
+    friendApi.beFriend(makeUserSimple(user), targetUser)
+    setUser({
+        ...user,
+        friends: user.friends ?
+        [...user.friends, targetUser]:
+        [targetUser]
+    })
+  }
+
+  const addFriend = async (targetUser: SimpleUserType) => {
+    // push보내고
+    setUser({
+        ...user,
+        requestFriends: user.requestFriends ?
+        [...user.requestFriends, targetUser]:
+        [targetUser]
+    })
+  }
+
+  if (!data.length) return null;
+
+  return (
+    <Container>
+      <Title>{title}</Title>
+      {data.slice(0, maxDisplayCount).map((item, index) => (
+        <FriendItem key={index} onClick={() => handleNative('프로필이동', item.id.toString())}>
+          <ProfileIcon />
+          <FriendInfo>
+            <FriendName>{item.name}</FriendName>
+            <MutualFriends>함께 아는 친구 2명</MutualFriends>
+          </FriendInfo>
+          <div style={{ flex: 1 }} />
+          {title === '신규 요청' ? (
+            <BeFriendButton onClick={() => beFriend(item)} />
+          ) : (
+            <AddFriendButton onClick={() => addFriend(item)} />
+          )}
+        </FriendItem>
+      ))}
+      {maxDisplayCount < data.length && <MoreButton onClick={handleShowMore}>더보기</MoreButton>}
+    </Container>
+  );
+};
+
+const AddFriendButton = ({ onClick}: {
+    onClick: () => void;
+}) => {
+    
+  return <ActionButton onClick={(e)=>{
+    e.stopPropagation()
+    onClick()
+  }}>추가</ActionButton>;
+};
+
+const BeFriendButton = ({onClick}: {
+    onClick: () => void;
+}) => {
+
+  return <ActionButton onClick={(e)=>{
+    e.stopPropagation()
+    onClick()
+  }}>수락</ActionButton>;
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 16px;
+  padding: 24px 15px;
+`;
+
+const Title = styled.h2`
+  margin-left: 8px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #919191;
+`;
+
+const FriendItem = styled.div`
+  display: flex;
+  gap: 12px;
+  padding: 8px;
+  background-color: black;
+  width: 100%;
+  cursor: pointer;
+`;
+
+const ProfileIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #909090;
+`;
+
+const FriendInfo = styled.div``;
+
+const FriendName = styled.p`
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const MutualFriends = styled.p`
+  color: #787878;
+  font-size: 13px;
+`;
+
+const MoreButton = styled.div`
+  text-align: center;
+  width: 100%;
+  padding: 4px;
+  margin-top: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: #787878;
+  background-color: black;
+  cursor: pointer;
+`;
+
+const ActionButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 32px;
+  padding: 0 16px;
+  margin-right: 4px;
+  font-size: 14px;
+  font-weight: bold;
+  color: black;
+  background-color: white;
+  border-radius: 9999px;
+  cursor: pointer;
+`;
+
+export default FriendSection;
--- src/pages/FriendPage/index.tsx
+++ src/pages/FriendPage/index.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled'
 import useRecommendFriends from '../../apis/queries/useRecommendFriends';
-import { UserType } from '../../types/user';
+import { SimpleUserType, UserType } from '../../types/user';
 import { friendApi } from '../../apis';
 import makeUserSimple from '../../utils/MakeUserSimple';
+import FriendSection from '../../components/FriendSection';
+import { Logo, NoPageContainer, NoText } from '../HomePage';
 
 export default function FriendPage ({
     user,
@@ -14,45 +16,43 @@ export default function FriendPage ({
 
     const {data} = useRecommendFriends(user)
 
+    if(!data) return <div>로딩중</div>
+
+    const isNotShowFriend = (friend: SimpleUserType) => {
+        const alreadyFriends = user.friends.find(userFriend => userFriend.id === friend.id)
+        const alreadyRequestFriends = user.requestFriends.find(userFriend => userFriend.id === friend.id)
+
+        return !alreadyFriends && !alreadyRequestFriends
+    }
+    const FilterData = data.filter(isNotShowFriend)
+
+    const isRecommendFriend = FilterData.length > 0
+
     return (
         <MainContainer>
-            <Logo>{data.length}</Logo>
-            <Logo onClick={()=>{
-                friendApi.beFriend(makeUserSimple(user), data[0])
-                setUser({
-                    ...user,
-                    friends: user.friends ?
-                    [...user.friends, data[0]]:
-                    [data[0]]
-                })
-            }}
-            >친구 수릭</Logo>
+            <FriendSection
+                title={'신규요청'}
+                data={user.receivedFriends}
+            />
+            <FriendSection
+                title={'학교 친구들'}
+                data={FilterData}
+            />
+            {
+                isRecommendFriend || <NoFriend />
+            }
         </MainContainer>
     );
 }
 
-const Logo = styled.div`
-    font-size: 2.5rem;
-    font-weight: 900;
-    color: #ffffff;
-`
-
-const SubText = styled.div`
-    font-size: 1rem;
-    margin-top: 15%;
-    margin-bottom: 5%;
-
-    font-weight: 400;
-    color: #ffffff;
-`
-
-const SubjectText = styled.div`
-    font-size: 2rem;
-    font-weight: 700;
-    color: #ffffff;
-
-    margin-bottom: 10%;
-`
+const NoFriend = () => {
+    return (
+        <NoPageContainer>
+            <NoText>추천 친구가 없어요 🥲</NoText>
+            <NoText>다른 친구를 초대해봐요</NoText>
+        </NoPageContainer>
+    )
+}
 
 const MainContainer = styled.div`
     height: 100vh;
