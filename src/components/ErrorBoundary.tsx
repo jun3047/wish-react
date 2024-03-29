8a6abecd971356a94b847edf58e7579a64a450ca
jeong jun <jungjun3047@naver.com>
2024-03-29 17:22:18 +0900

    fix: 재시도 버튼 추가

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Logo, MainContainer, NoPageContainer, NoText } from '../pages/HomePage';
+import styled from '@emotion/styled';
 
 interface Props {
   children: React.ReactNode;
@@ -34,13 +35,18 @@ class ErrorBoundary extends React.Component<Props, State> {
     this.setState({ isOnline: navigator.onLine });
   }
 
+  retry = () => {
+    this.setState({ hasError: false });
+    window.location.reload();
+  }
+
   renderContent() {
     if (this.state.hasError) {
-      return <ErrorPage />;
+      return <ErrorPage onRetry={this.retry} />;
     }
 
     if(!this.state.isOnline) {
-      return <NoNetworkPage />;
+      return <NoNetworkPage onRetry={this.retry} />;
     }
     return this.props.children;
   }
@@ -50,7 +56,9 @@ class ErrorBoundary extends React.Component<Props, State> {
   }
 }
 
-const NoNetworkPage = () => {
+const NoNetworkPage = ({onRetry}: {
+  onRetry: () => void;
+}) => {
 
   return (
       <MainContainer>
@@ -59,12 +67,15 @@ const NoNetworkPage = () => {
               <NoText>📡</NoText>
               <NoText></NoText>
               <NoText>네트워크를 연결해주세요</NoText>
+              <RetryButton onClick={onRetry}>다시 시도하기</RetryButton>
           </NoPageContainer>  
       </MainContainer>
   )
 }
 
-const ErrorPage = () => {
+const ErrorPage = ({onRetry}: {
+  onRetry: () => void;
+}) => {
 
   return (
       <MainContainer>
@@ -74,9 +85,26 @@ const ErrorPage = () => {
               <NoText></NoText>
               <NoText>다시 시작해도 문제가 계속되면</NoText>
               <NoText>문의해주세요 😭</NoText>
+              <RetryButton onClick={onRetry}>다시 시도하기</RetryButton>
           </NoPageContainer>  
       </MainContainer>
   )
 }
+
+const RetryButton = styled.button`
+
+  position: absolute;
+  bottom: 20%;
+
+  border-radius: 5px;
+  padding: 10px 20px;
+  margin-top: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  color: black;
+  font-weight: 400;
+
+  background-color: white;
+`
 
 export default ErrorBoundary;
