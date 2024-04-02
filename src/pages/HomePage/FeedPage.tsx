1c680c006263a9d366c24f3a59b660903a48247e
jeong jun <jungjun3047@naver.com>
2024-04-02 18:57:25 +0900

    test: feed에 아무것도 안 뜨는 현상 확인

--- src/pages/HomePage/FeedPage.tsx
+++ src/pages/HomePage/FeedPage.tsx
@@ -12,10 +12,10 @@ import { useState } from "react"
 
 export default function FeedPage ({user}: {user: UserType}) {
 
-    const {data} = useRecommendFeeds(user)
+    const {data} = useRecommendFeeds(user) 
     const [warnFeedIds, setWarnFeedIds] = useState<number[]>([])
 
-    alert('useRecommendFeeds(user)Data: ' + JSON.stringify(data))
+    alert('useRecommendFeeds(user)Data: ' + data.length + JSON.stringify(data))
 
     if(data.length === 0) return <NoFeedPage />
 
@@ -25,7 +25,7 @@ export default function FeedPage ({user}: {user: UserType}) {
         (feed.warnUserIds.length === 0 || !(feed.warnUserIds as number[]).includes(user.id))
     )
 
-    alert('filteredData: ' + JSON.stringify(data))
+    alert('filteredData: ' + data.length + JSON.stringify(data))
 
     if(filteredData.length === 0) return <NoFeedPage />
 
